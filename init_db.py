a8d7ad4f3151d558d576dcaf00194e2b80bdc3ba
Luiz Bessa <lbessaregis@gmail.com>
2023-12-11 11:43:39 -0300

    Fix relationship in Favorite and column names

--- init_db.py
+++ init_db.py
@@ -16,26 +16,34 @@ cursor.execute('CREATE TABLE IF NOT EXISTS Person (id SERIAL PRIMARY KEY,'
                 'birth_date DATE NOT NULL,'
                 'password VARCHAR(20) NOT NULL);')
 
+cursor.execute('CREATE TABLE IF NOT EXISTS Pix ('
+                'id SERIAL PRIMARY KEY,'
+                'person_id INTEGER NOT NULL,'
+                'type VARCHAR(10) NOT NULL,'
+                'key VARCHAR(256) NOT NULL);')
+
 cursor.execute('CREATE TABLE IF NOT EXISTS Payment (id SERIAL PRIMARY KEY,'
-                'id_sender int NOT NULL,'
-                'id_receiver int NOT NULL,'
+                'sender_id int NOT NULL,'
+                'receiver_id int NOT NULL,'
                 'value float NOT NULL,'
                 'date DATE NOT NULL);')
 
 cursor.execute('CREATE TABLE IF NOT EXISTS Favorite (id SERIAL PRIMARY KEY,'
-                'id_user int NOT NULL,'
-                'id_favorite int NOT NULL,'
-                'pix VARCHAR(255) NOT NULL);')
+                'person_id int NOT NULL,'
+                'pix_id int NOT NULL);')
+
+cursor.execute('ALTER TABLE Pix '
+                'ADD FOREIGN KEY (person_id) REFERENCES Person(id); ')
 
 cursor.execute('ALTER TABLE Payment '
-                'ADD FOREIGN KEY (id_sender) REFERENCES Person(id); '
+                'ADD FOREIGN KEY (sender_id) REFERENCES Person(id); '
                 'ALTER TABLE Payment '
-                'ADD FOREIGN KEY (id_receiver) REFERENCES Person(id);')
+                'ADD FOREIGN KEY (receiver_id) REFERENCES Person(id);')
 
 cursor.execute('ALTER TABLE Favorite '
-                'ADD FOREIGN KEY (id_user) REFERENCES Person(id); '
+                'ADD FOREIGN KEY (person_id) REFERENCES Person(id); '
                 'ALTER TABLE Favorite '
-                'ADD FOREIGN KEY (id_favorite) REFERENCES Person(id);')
+                'ADD FOREIGN KEY (pix_id) REFERENCES Pix(id);')
 
 conn.commit()
 cursor.close()
