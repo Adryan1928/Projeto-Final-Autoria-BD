0a0667aadcb51e97a7e6ad52e82b62dd3e745689
Adryan1928 <adryanoliveira1928@gmail.com>
2023-12-17 15:05:06 -0300

    Entradas e saidas

--- utils.py
+++ utils.py
@@ -2,5 +2,11 @@ from models.utils import transaction
 
 @transaction
 def getPayments(cursor, id):
-    cursor.execute('')
+    cursor.execute('SELECT * FROM payment WHERE receiver_id = {}'.format(id))
+    Entredas = cursor.fetchall()
+
+    cursor.execute('SELECT * FROM payment WHERE sender_id = {}'.format(id))
+    Saidas = cursor.fetchall()
+
+    payments = {'Entradas': Entredas, 'Saidas': Saidas}
     return payments
