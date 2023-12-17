b689a3406506d23d87a452d65965f767f7260576
Adryan1928 <adryanoliveira1928@gmail.com>
2023-12-17 16:04:18 -0300

    Tela de extrato está pronta(Apenas a barra de pesquisa está funcionando)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from controllers.users import auth
 import utils
 
@@ -17,10 +17,23 @@ def pagamentos(id):
     payments = utils.getPayments(id)
     return render_template('pagamentos.html', post = id, posts = payments)
 
-@app.route('/extrato/<int:id>/')
+@app.route('/extrato/<int:id>/', methods=['GET', 'POST'])
 def extrato(id):
     payments = utils.getPayments(id)
-    return render_template('extrato.html', posts=payments)
+    if request.method == 'POST':
+        text = request.form.get('filtro')
+        if len(text.strip()) == 0:
+            return render_template('extrato.html', post=id, posts=payments)
+        
+        payments_filtrado = {'Entradas': [], 'Saidas': []}
+        for en_sa in payments:
+            for payment in payments[en_sa]:
+                if payment['name'] == text:
+                    payments_filtrado[en_sa].append(payment)
+        
+        return render_template('extrato.html', post=id, posts=payments_filtrado)
+    
+    return render_template('extrato.html', post=id, posts=payments)
 
 @app.route('/pix/', methods=['GET', 'POST'])
 def pix():
